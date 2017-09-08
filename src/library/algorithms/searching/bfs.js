a89fbe69f573a8d0d5c21061e976497d9095b370
simeonlees <simeon.lees@sandstone-tech.com>
2017-09-08 16:03:59 +0100

    Added BFS and tests

---
+++ src/library/algorithms/searching/bfs.js
@@ -0,0 +1,26 @@
+import { Graph } from './../../data-structures/graph';
+
+/**
+ * Graph searching algorithm that enqueues all adjacent nodes then
+ * dequeues and visits the first node
+ */
+export class BFS extends Graph {
+  breadthFirstSearch(callback, node, visited = [], queue = []) {
+    if (callback(this._graph[node].value)) {
+      return this._graph[node].value;
+    }
+    visited.push(node);
+    for (let connection of this._graph[node].connections) {
+      if (!visited.includes(connection) && !queue.includes(connection)) {
+        queue.push(connection);
+      }
+    }
+    // let val = this.breadthFirstSearch(callback, queue.shift(), visited, queue);
+    // if (val) {
+    //   return val;
+    // }
+    // return null;
+
+    return queue.length > 0 ? this.breadthFirstSearch(callback, queue.shift(), visited, queue) : null;
+  }
+}
